76890eb13e5631b12bda84a98e0035e8e166f5c1
felipegrossilopes <felipe.grossi11@gmail.com>
2023-03-27 13:21:18 -0300

    adicionando a lista git adicionando

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 class Produto{
-
     constructor(){
         this.id = 1;
-    }
+        this.arrayProdutos =[];
 
+    }
     Adicionar(){
         let produto = this.lerDados()
-        
+        if(this.Validar(produto)== true){
+            this.Salvar(produto)
+        }
+        this.Listar()
     }
 
     lerDados(){
-         let produto = {}
+         let produto ={}
         
-         produto.id = this.id;
-         produto.nomeProduto = document.getElementById('pdnome')
-         produto.precoProduto = document.getElementById('pdpreco')
+         produto.id = this.id
+         produto.nomeProduto = document.getElementById('pdnome').value
+         produto.precoProduto = document.getElementById('pdpreco').value
 
          return produto
 
     }
-        validar(produto){
+        Validar(produto){
             let msg = '';
 
             if(produto.nomeProduto == ''){
@@ -30,12 +33,39 @@ class Produto{
             if(produto.precoProduto == ''){
                 msg += 'Por favor, insira corretamente o preco do produto" \n'
             }
-            if(msg != '')
+            if(msg != ''){
                 alert(msg)
                 return false
+            }
+            return true
         }
-        return true
+        Salvar(produto){
+            this.arrayProdutos.push(produto)
+            this.id++;
+        }
+        
+        Listar(){
+            let tbody = document.getElementById('tbody')
+            tbody.innerText =''
+
+            for(let i = 0; i < this.arrayProdutos.length; i++){
+
+                let tr = tbody.insertRow();
+
+                let td_id = tr.insertCell();
+                let td_nome = tr.insertCell();
+                let td_preco = tr.insertCell();
+                let td_del = tr.insertCell();
+
+                td_id.innerText = this.arrayProdutos[i].id;
+                td_nome.innerText = this.arrayProdutos[i].nomeProduto;
+                td_preco.innerText = this.arrayProdutos[i].precoProduto;
+                let imagem = document.createElement('img')
+                imagem.src = 'del.png'
+                td_del.appendChild(imagem)
+        }
 }
 
-var produto = new Produto();
+}
+    var produto = new Produto()
 
